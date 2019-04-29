67c86cf83836f0f6cb397227d930725e8b7cf572
Norris <feixu@tju.edu.cn>
2019-04-29 10:29:12 +0800

    commit

---
+++ src/utils/request.js
@@ -0,0 +1,34 @@
+import wx from './wx'
+import Fly from 'flyio'
+
+const request = new Fly()
+
+request.config.timeout = 5000
+
+if (process.env.NODE_ENV === 'production') {
+  request.config.baseURL = 'https://www.niltouch.cn/sesame/api'
+} else {
+  request.config.baseURL = 'http://localhost:3000/sesame/api'
+}
+
+request.interceptors.request.use((request) => {
+  wx.showLoading({ title: 'Loading...' })
+  return request
+})
+
+request.interceptors.response.use(
+  (response, promise) => {
+    wx.hideLoading()
+    return promise.resolve(response.data)
+  },
+  (err, promise) => {
+    wx.hideLoading()
+    wx.showToast({
+      title: err.message,
+      icon: 'none'
+    })
+    return promise.resolve()
+  }
+)
+
+export default request
