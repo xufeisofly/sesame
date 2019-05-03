70379d32920cc1118208bd275be4d0e119968962
Norris <feixu@tju.edu.cn>
2019-05-03 21:30:05 +0800

    city list

--- src/pages/cities/index.vue
+++ src/pages/cities/index.vue
@@ -7,15 +7,41 @@
     ></van-search>
 
     <div id="position" class="city-block">
-      <p>定位/历史</p>
+      <p class="city-bar">定位</p>
       <div class="city-list">
         <van-row>
-          <van-col span="8" v-for="city in cities" :key="city.id">
-            <div class="city-item">
-              <van-button @click="selectCity(city.name)">{{ city.name }}</van-button>
+          <van-col span="6" v-for="city in positionCities" :key="city.id">
+            <div class="city-item"
+                 @click="selectCity(city.name)">
+              {{ city.name }}
             </div>
           </van-col>
         </van-row>
+      </div>
+    </div>
+
+    <div id="hot-city" class="city-block">
+      <p class="city-bar">热门城市</p>
+      <div class="city-list">
+        <van-row>
+          <van-col span="6" v-for="city in hotCities" :key="city.id">
+            <div class="city-item"
+                 @click="selectCity(city.name)">
+              {{ city.name }}
+            </div>
+          </van-col>
+        </van-row>
+      </div>
+    </div>
+
+    <div id="city-list" class="city-list">
+      <div class="cabulary-list" v-for="(letter, indexL) in letters" :key="letter">
+        <div class="letter-label">
+          <p class="city-bar">{{ letter }}</p>
+        </div>
+        <div class="city-list-item-wrapper" v-for="(city, indexC) in vacabCities[letter]" :key="city.id">
+          <p class="city-list-item" @click="selectCity(city.name)">{{ city.name }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -29,11 +55,23 @@ export default {
   data () {
     return {
       inputValue: null,
-      cities: [
+      positionCities: [
+        { id: 1, name: '北京' }
+      ],
+      hotCities: [
         { id: 1, name: '北京' },
         { id: 2, name: '上海' },
         { id: 3, name: '广州' }
-      ]
+      ],
+      vacabCities: {
+        B: [
+          { id: 1, name: '北京' }
+        ],
+        S: [
+          { id: 2, name: '上海' }
+        ]
+      },
+      letters: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     }
   },
   mounted () {
@@ -59,5 +97,27 @@ export default {
 <style scoped>
 .city-item {
   text-align: center;
+  font-size: 14px;
+  border: 0.5px solid #ccc;
+  margin: 3px 5px;
+  padding: 3px 0;
+}
+
+.city-bar {
+  font-size: 10px;
+  display: block;
+  background-color: #ccc;
+  margin: 0;
+}
+
+.city-list-item-wrapper {
+  margin: 0, 10px;
+  margin-left: 10px;
+  margin-right: 10px;
+  border-bottom: 0.1px #ccc solid;
+}
+
+.city-list-item {
+  font-size: 14px;
 }
 </style>
